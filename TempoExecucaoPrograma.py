e30f3b4c7de683e4baf5a8ce9a99a524a5a2cb0f
FaelsNunes <fael_nunes.rnb@hotmail.com>
2024-02-20 22:45:17 -0300

    Atualização minhas funções e primeira versão do Tempo de Execução Programa

---
+++ TempoExecucaoPrograma.py
@@ -0,0 +1,35 @@
+'''
+Programa destinado a ser copiado para outros programas para poder mostrar o tempo de execução
+Está puxando as funções do meu pacote, por via das dúvidas estou incluind abaixo as duas funções aqui
+'''
+from FuncoesRnb import dates, strings
+
+'''
+def data_hora_atual():
+    import datetime
+    agora = datetime.datetime.now() #pega a data e hora atual
+    agora = datetime.datetime(agora.year,agora.month,agora.day,agora.hour,agora.minute,agora.second)
+    return agora
+    
+##########################################################################################################
+
+def titulo(msg):
+    print('-=' * ((len(msg) // 2) + 2))
+    print('  {:^}'.format(msg))
+    print('-=' * ((len(msg) // 2) + 2))
+
+
+'''
+
+inicioexecucao = dates.data_hora_atual()
+
+'''
+Escrever o programa aqui
+'''
+
+fimexecucao = dates.data_hora_atual()
+tempoexecucao = (fimexecucao - inicioexecucao)
+strings.titulo('DETALHES DA EXECUÇÃO')
+print(f'Início da execução: {inicioexecucao}')
+print(f'Fim da execução: {fimexecucao}')
+print(f'Duração da execução: {tempoexecucao}')
